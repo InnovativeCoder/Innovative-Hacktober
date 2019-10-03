5ce1026fae5d3a500e7d4200f42c8c4b6107fd3c
kscoder29 <42241932+kscoder29@users.noreply.github.com>
2019-10-03 18:53:04 +0530

    Use the above python script to send email from gmail to multiple users.

---
+++ Python/smtp_mailer_to_Multiple_users.py
@@ -0,0 +1,18 @@
+#Python code to send emails to multiple users
+
+import smtplib
+
+#List of email_ids to send the mail
+#receiver1@example.com can be any email id to which you want the email to be sent
+
+mail_list=["receiver1@example.com","receiver2@example.com","receiver3@example.com"]
+for i in range(len(mail_list)):
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('sender@example.com','senders_password')
+    msg='Hello!'
+    server.sendmail('sender@example.com',mail_list[i],msg)
+    server.quit()
+
+
+#sender@example.com can be any email id from which you want to send the email
