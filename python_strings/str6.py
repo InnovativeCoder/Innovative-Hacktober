bb8c4b3aeec62575941fed0998524d3d940efa66
F9Uf <atoseka@gmail.com>
2019-10-04 18:00:12 +0700

    print string 1k time without loop

--- python_strings/str6.py
+++ python_strings/str6.py
@@ -1,7 +1,7 @@
 # print "hello world" 1k time without for loop
 
 # solution 1
-# print("helloworld\n" * 1000)
+print("helloworld\n" * 1000)
 
 # solution 2
 x = "xxxxxxxxxx".replace("x", "xxxxxxxxxx").replace("x", "xxxxxxxxxx")
