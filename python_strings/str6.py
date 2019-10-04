0dadcdcabb6720be737d921e36a95783e77f03eb
F9Uf <atoseka@gmail.com>
2019-10-04 17:58:19 +0700

    add str6.py print 'helloworld' 1k time without loop

---
+++ python_strings/str6.py
@@ -0,0 +1,8 @@
+# print "hello world" 1k time without for loop
+
+# solution 1
+# print("helloworld\n" * 1000)
+
+# solution 2
+x = "xxxxxxxxxx".replace("x", "xxxxxxxxxx").replace("x", "xxxxxxxxxx")
+print(x.replace("x", "helloworld\n"))
