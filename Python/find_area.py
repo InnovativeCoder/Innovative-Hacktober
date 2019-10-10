82e0d52fa5933b5f16d844c02139a980c2ceec3e
Brian Hopper <brian.hopper@transdev.com>
2019-10-10 16:25:21 -0700

    Update find_area.py
    Added a couple ways of calculating the area of triangles.

--- Python/find_area.py
+++ Python/find_area.py
@@ -5,6 +5,8 @@ def get_area(shape):
         rectangle_area()
     elif shape=="circle":
         circle_area()
+    elif shape=="triangle":
+        triangle_area()
     else:
             print("plz enter rectangle or area")
 def rectangle_area():
@@ -16,6 +18,23 @@ def circle_area():
     r=float(input("enter the radius"))
     area=math.pi*(math.pow(r,2))
     print("the area of circle is {:.2f}".format(area))
+def triangle_area():
+    print("There are two ways to find the area of a triangle.  Chose one:")
+    print("1. Base and height")
+    print("2. Length of all three sides")
+    choice=input("Which way? ")
+    if choice=="1":
+        b=float(input("Enter the length of the base "))
+        h=float(input("Enter the length of the height "))
+        area=b*h/2
+        print("The area of the triangle is {:.2f}".format(area))
+    if choice=="2":
+        a=float(input("Enter the length of side a "))
+        b=float(input("Enter the length of side b "))
+        c=float(input("Enter the length of side c "))
+        p=(a+b+c)/2
+        area = math.sqrt(p*(p-a)*(p-b)*(b-e))
+        print("The area of the triangle is {:.2f}".format(area))
 
 def main():
     shape_type=input("enter shape")
