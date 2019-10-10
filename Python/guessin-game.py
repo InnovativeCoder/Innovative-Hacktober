5938bc98a236983c0c4303931de610fe0d29e4d3
Brian Hopper <brian.hopper@transdev.com>
2019-10-10 16:43:27 -0700

    Update guessin-game.py
    Added code to allow the game to count the number of guesses, and cleaned up the output a bit so there are blank lines between guesses for readability.

--- Python/guessin-game.py
+++ Python/guessin-game.py
@@ -1,19 +1,17 @@
 import random
-
 number = random.randint(1,99)
-
-guess = int(input('Enter an integer from 1 to 99:'))
-
-while number != 'guess':
-    if guess < number:
-        print('guess is low')
-        guess = int(input('Enter an integer from 1 to 99:'))
+chance = 0
+correct = False
+while not correct:
+    guess = int(input('Enter an integer from 1 to 99: '))
+    chance += 1
+    if guess < 0:
+        print('guess isn't a negative number\n')
+    elif guess < number:
+        print('guess is low\n')
     elif guess > number:
-        print('guess is high')
-        guess = int(input('Enter an integer from 1 to 99:'))
-    elif guess < 0:
-        print("guess isn't a negative number")
-        guess = int(input('Enter an integer from 1 to 99:'))
-    else:
-        print('congratulations,you guessed it. the secret number is {}'.format(number))
-        break
+        print('guess is high\n')
+    elif guess == number:
+        correct=True
+print('congratulations,you guessed it. the secret number is {}!'.format(number))
+print('And it took you {} guesses!'.format(chance))
