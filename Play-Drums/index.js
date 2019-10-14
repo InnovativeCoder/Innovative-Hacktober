e348cdb2e92b370c4dd9ae77b6f994f07ca5501e
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2019-10-14 21:08:45 +0530

    (Play-drums)added new sounds,new keys assigned to drums
    1.highhat,cymbal added to the drum kit.
    
    2.new keys assigned to each drum piece
    crash(r)
    cymbal(s)
    kick(v)
    snare(c)
    high hat(d)
    tom1(g)
    tom2(h)
    floor tom1(j)
    floor tom2(k)
    
    3.Layout from L to R is as follows:
    crash,highhat,snare,kick,tom1,tom2,floor tom1,floor tom2,cymbal

--- Play-Drums/index.js
+++ Play-Drums/index.js
@@ -18,35 +18,51 @@ buttonAnimation(e.key);
 function makeSound(sound)
 {
     switch (sound) {
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
+        case "r":
+            var crash = new Audio("sounds/crash rock.flac");
+            var kick = new Audio("sounds/kick-bass.mp3");
+            crash.play();
+            kick.play();
             break;
-        case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
+            
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
+            var highhat = new Audio("sounds/highhat(rock).flac");
+            highhat.play();
             break;
-        case "j":
+
+        case "c":
             var snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
 
-        case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
+        case "v":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
             break;
 
-        case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
+        case "g":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "h":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "j":
+            var ftom1 = new Audio("sounds/ftom-1.mp3");
+            ftom1.play();
+            break;
+        
+        case "k":
+            var ftom2 = new Audio("sounds/ftom-2.mp3");
+            ftom2.play();
+            break;
+        
+        case "s":
+            var cymbal = new Audio("sounds/cymbal rock.flac");
+            cymbal.play();
             break;
 
         default: console.log(sound);
@@ -58,5 +74,5 @@ function buttonAnimation(currentKey)
     var actvbtn=document.querySelector("."+currentKey);
     actvbtn.classList.add("pressed");
     setTimeout(function() {        
-        actvbtn.classList.remove("pressed");},100);
+        actvbtn.classList.remove("pressed");},90);
 }
