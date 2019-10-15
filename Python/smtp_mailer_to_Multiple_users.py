8245fe7dcb74905f56853e4f34b22ed3a89be842
im-alexandre <48188379+im-alexandre@users.noreply.github.com>
2019-10-15 10:42:45 -0300

    Change iter in list
    It's not necessary to use index to iter pythonic lista.

--- Python/smtp_mailer_to_Multiple_users.py
+++ Python/smtp_mailer_to_Multiple_users.py
@@ -6,12 +6,12 @@ import smtplib
 #receiver1@example.com can be any email id to which you want the email to be sent
 
 mail_list=["receiver1@example.com","receiver2@example.com","receiver3@example.com"]
-for i in range(len(mail_list)):
+for i in mail_list:
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login('sender@example.com','senders_password')
     msg='Hello!'
-    server.sendmail('sender@example.com',mail_list[i],msg)
+    server.sendmail('sender@example.com',i ,msg)
     server.quit()
 
 
