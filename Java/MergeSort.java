b8405d502d3a741cba69311809bb8e0e3a3f60c9
vishakha0505 <53859055+vishakha0505@users.noreply.github.com>
2019-10-16 02:17:34 -0700

    Update MergeSort.java

--- Java/MergeSort.java
+++ Java/MergeSort.java
@@ -1,96 +1,72 @@
-//Program to sort elements using Mergesort
-
-public class MergeSort {
-	
-	void sort(int arr[], int left, int right) 
-	{ 
-		if (left < right) 
-		{ 
-			
-			int mid = (left+right)/2; 
-
-			sort(arr, left, mid);  //Sort both the halves
-			sort(arr , mid+1, right); 
-
-			merge(arr, left, mid, right); //Merge the sorted halves
-		} 
-	} 
-	void merge(int arr[], int l, int m, int h) 
-	{ 
-		
-		int n1 = m - l + 1; //Calculate size of both the arrays
-		int n2 = h - m; 
-
-	
-		int L[] = new int [n1]; 
-		int R[] = new int [n2]; 
-
-		for (int i=0; i<n1; ++i) 
-			L[i] = arr[l + i]; 
-		for (int j=0; j<n2; ++j) 
-			R[j] = arr[m + 1+ j]; 
-
-		int i = 0, j = 0; 
-		int k = l;
-		
-		while (i < n1 && j < n2) //Copy the smallest element into arr[]
-		{ 
-			if (L[i] <= R[j]) 
-			{ 
-				arr[k] = L[i]; 
-				i++; 
-			} 
-			else
-			{ 
-				arr[k] = R[j]; 
-				j++; 
-			} 
-			k++; 
-		} 
-
-		//Copy remaining elements of L[] if present
-		while (i < n1) 
-		{ 
-			arr[k] = L[i]; 
-			i++; 
-			k++; 
-		} 
-
-		//Copy remaining elements of R[] if present
-		while (j < n2) 
-		{ 
-			arr[k] = R[j]; 
-			j++; 
-			k++; 
-		} 
-	} 
-
-	static void display(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i] + " "); 
-	} 
-	
-	public static void main(String[] args) {
-		int arr[] = {15, 11, 10, 5, 2, 7}; 
-
-		System.out.println("Input array:"); 
-		display(arr); 
-
-		MergeSort obj = new MergeSort(); 
-		obj.sort(arr, 0, arr.length-1); 
-
-		System.out.println("\nSorted array:"); 
-		display(arr); 
-
-	}
+public class MergeSortExample
+{  
+void merge(int arr[], int beg, int mid, int end)  
+{  
   
-}
-
- /*Output
-  Input array:
-  15 11 10 5 2 7 
-  Sorted array:
-  2 5 7 10 11 15 
-  */
+int l = mid - beg + 1;  
+int r = end - mid;  
+  
+intLeftArray[] = new int [l];  
+intRightArray[] = new int [r];  
+  
+for (int i=0; i<l; ++i)  
+LeftArray[i] = arr[beg + i];  
+  
+for (int j=0; j<r; ++j)  
+RightArray[j] = arr[mid + 1+ j];  
+  
+  
+int i = 0, j = 0;  
+int k = beg;  
+while (i<l&&j<r)  
+{  
+if (LeftArray[i] <= RightArray[j])  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+}  
+else  
+{  
+arr[k] = RightArray[j];  
+j++;  
+}  
+k++;  
+}  
+while (i<l)  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+k++;  
+}  
+  
+while (j<r)  
+{  
+arr[k] = RightArray[j];  
+j++;  
+k++;  
+}  
+}  
+  
+void sort(int arr[], int beg, int end)  
+{  
+if (beg<end)  
+{  
+int mid = (beg+end)/2;  
+sort(arr, beg, mid);  
+sort(arr , mid+1, end);  
+merge(arr, beg, mid, end);  
+}  
+}  
+public static void main(String args[])  
+{  
+intarr[] = {90,23,101,45,65,23,67,89,34,23};  
+MyMergeSort ob = new MyMergeSort();  
+ob.sort(arr, 0, arr.length-1);  
+  
+System.out.println("\nSorted array");  
+for(int i =0; i<arr.length;i++)  
+{  
+    System.out.println(arr[i]+"");  
+}  
+}  
+}  
