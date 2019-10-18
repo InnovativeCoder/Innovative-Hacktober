e701de0e002dd59c8796f0b8a356d01860548f1b
SyntaxError-exe <56209259+SyntaxError-exe@users.noreply.github.com>
2019-10-18 17:13:48 -0500

    Update str5.py

--- python_strings/str5.py
+++ python_strings/str5.py
@@ -6,3 +6,6 @@ print(str)
 value = 30
 str = f'This another way of a string with a value {value}'
 print(str)
+
+Umm, I'm the one that made this pull request, you deleted it and said there were conflicts but there weren't cause you then merged it back into the master
+Because of this, I lost one of my pull request and had to start over my review period, please don't try to do that again, thanks.
