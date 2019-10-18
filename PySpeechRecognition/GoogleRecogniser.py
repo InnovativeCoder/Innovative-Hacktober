3ceb2272c50b282be770abd1a58023a6458f6cfe
Shiva Sankeerth Reddy <29270279+ShivaSankeerth@users.noreply.github.com>
2019-10-18 17:41:22 +0530

    Update GoogleRecogniser.py
    Updated GoogleRecogniser.py to meet python PEP guidelines

--- PySpeechRecognition/GoogleRecogniser.py
+++ PySpeechRecognition/GoogleRecogniser.py
@@ -2,14 +2,14 @@ import speech_recognition as sr
 from Recorder import record_audio
 
 def RecognizeSpeechGoogle(AUDIO_FILENAME, num_seconds=5):
-    r = sr.Recognizer()
+    recogniser = sr.Recognizer()
     record_audio(num_seconds,AUDIO_FILENAME)
     audio_file = sr.AudioFile(AUDIO_FILENAME)
     with audio_file as source:
-        r.adjust_for_ambient_noise(source)
-        audio=r.record(source)
+        recogniser.adjust_for_ambient_noise(source)
+        audio = recogniser.record(source)
     try:
-        text=r.recognize_google(audio)
+        text = recogniser.recognize_google(audio)
         return text
     except:
         return ""
