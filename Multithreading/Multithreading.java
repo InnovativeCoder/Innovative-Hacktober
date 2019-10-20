7919bd40497895d6f934a4ed40fa4947b22deb7e
Mrudula Acharya <mrudu101200@gmail.com>
2019-10-20 17:31:08 +0530

    Multithreading Code

---
+++ Multithreading/Multithreading.java
@@ -0,0 +1,50 @@
+class thread1 extends Thread
+{
+  public void run()
+   {
+     try
+      {
+       for(int i=0;i<20;i++)
+        {
+          if(i%2==0)
+         { System.out.println(i);
+          sleep(1000);
+         }
+        }
+      }
+     catch (Exception e)
+      {
+         System.out.println("Exception Occurs");
+      }
+    }
+ }
+class thread2 extends Thread
+{
+  public void run()
+   {
+     try
+      {
+       for(int i=0;i<20;i++)
+        {
+          if(i%2!=0)
+         { System.out.println(i);
+          sleep(1000);
+         }
+        }
+      }
+     catch (Exception e)
+      {
+         System.out.println("Exception Occurs");
+      }
+    }
+ }
+class Multithreading
+  {
+    public static void main(String args[])
+     {
+        thread1 a=new thread1();
+        a.start();
+        thread2 b=new thread2();
+        b.start();
+}
+}
