4eff5ae00e4005177975e303c42c61e8dd0d939d
Ally0601 <44461139+Ally0601@users.noreply.github.com>
2019-10-20 12:04:09 +0530

    Multithreading

---
+++ Multithreading/Multithreading.java
@@ -0,0 +1,55 @@
+class thread1 extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			for(int i=0;i<20;i++)
+			{
+				if(i%2==0)
+				{
+					System.out.println("Even number: "+i);
+					sleep(1000);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occurs");
+		}
+	}
+}
+
+class thread2 extends Thread
+{
+	public void run()
+	{
+		try
+		{
+			for(int i=0;i<20;i++)
+			{
+				if(i%2!=0)
+				{
+					System.out.println("Odd number: "+i);
+					sleep(1000);	
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occurs");
+		}
+	}
+}
+
+class Multithreading
+{
+	public static void main(String[] args)
+	{
+		thread1 a=new thread1();
+		a.start();
+		thread2 b=new thread2();
+		b.start();
+	}
+}
+
