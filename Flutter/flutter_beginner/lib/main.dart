09f5a12287b4260712a721513be29aadc3658d77
Earth <pimanus1234@gmail.com>
2019-10-23 18:08:11 +0700

    Complete example project from flutter docs

--- Flutter/flutter_beginner/lib/main.dart
+++ Flutter/flutter_beginner/lib/main.dart
@@ -14,6 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+        theme: ThemeData(          
+        primaryColor: Colors.pinkAccent,
+      ),                        
       home: RandomWords(),
     );
   }
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
 // #docregion RWS-var
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();   
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   // #enddocregion RWS-var
 
@@ -45,28 +48,28 @@ class RandomWordsState extends State<RandomWords> {
   // #enddocregion _buildSuggestions
 
   // #docregion _buildRow
-Widget _buildRow(WordPair pair) {
-  final alreadySaved = _saved.contains(pair);
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-    trailing: Icon(
-      alreadySaved ? Icons.favorite : Icons.favorite_border,
-      color: alreadySaved ? Colors.red : null,
-    ),
-    onTap: () {
-      setState(() {
-        if (alreadySaved) {
-          _saved.remove(pair);
-        } else {
-          _saved.add(pair);
-        }
-      });
-    },
-  );
-}
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
   // #enddocregion _buildRow
 
   // #docregion RWS-build
@@ -74,12 +77,46 @@ Widget _buildRow(WordPair pair) {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startupsss Name Generator'),
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
   // #enddocregion RWS-build
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
   // #docregion RWS-var
 }
 // #enddocregion RWS-var
