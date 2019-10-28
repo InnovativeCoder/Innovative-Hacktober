08b89d63fce5455ddab087845e5965ffe9dc2112
Earth <pimanus1234@gmail.com>
2019-10-28 21:00:07 +0700

    firebase crud

---
+++ Firebase/README.md
@@ -0,0 +1,7 @@
+This is firebase project I learned fron The Net Ninja youtube channel.
+Don't forget toadd your project configuration on index.html
+
+# firebase-firestore-playlist
+All course files for the Firebase Firestore playlist on The Net Ninja YouTube channel. 
+
+Make sure you select the correct branch to see the files :)
---
+++ Firebase/app.js
@@ -0,0 +1,57 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+// create element and render cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    cross.textContent = 'x';
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+
+    cafeList.appendChild(li);
+    //Delete data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+}
+
+//Collect data from firebase // where('name','==','variable') = where in php
+// db.collection('cafes').orderBy('name').get().then((snapshot) => {
+//    snapshot.docs.forEach(doc => {
+//        renderCafe(doc);
+//    })
+//})
+
+
+// Add data 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+        //form จากที่ประกาศเริ่มต้นไฟล์.name จาก Input typeในหน้า Index.รับค่า
+    });
+    form.name.value = '';
+    form.city.value = '';
+});
+//real-time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            renderCafe(change.doc)
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
+})
