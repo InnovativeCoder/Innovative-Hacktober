18b1ab177e1cda66320b2dae7936e9e9f0a1bf6b
ankit1ag <ankit1ag@gmail.com>
2020-07-17 21:11:55 +0530

    height balanced binary tree

--- Java/mergeSort.java
+++ Java/mergeSort.java
@@ -1,49 +1,72 @@
-import java.util.Scanner;
-class MergeSort {
-	public static void main(String[] args) {
-		Scanner sobj = new Scanner(System.in);
-		System.out.println("Enter the total number of numbers you want to sort");
-		int n = sobj.nextInt();
-		int arr[] = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i] = sobj.nextInt();
-		}
-		sobj.close();
-	   	Sort sort = new Sort();
-		sort.mergeSort(arr,0,n-1);
-		for(int i=0;i<n;i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
-}
-class Sort {
-	public void mergeSort(int arr[], int x, int n) {
-		if(x<n) {
-			mergeSort(arr, x, (x+n)/2);
-			mergeSort(arr, (x+n)/2+1, n);
-			merge(arr, x, n);
-		}
-	}
-	public void merge(int arr[], int init, int n) {
-		int x1 = init;
-		int x2 = (init+n)/2+1;
-		int x = init;
-		int arr2[] = arr.clone();
-		while (x1 < (init+n)/2+1 && x2 <= n) {
-			if(arr2[x1] < arr2[x2]) {
-				arr[x++] = arr2[x1++];
-			}
-			if(arr2[x2] < arr2[x1]) {
-				arr[x++] = arr[x2++];
-			}
-		}
-		while(x1<(init+n)/2+1) {
-			arr[x++]=arr2[x1++];
-		}
-		while(x2<=n) {
-			arr[x++]=arr2[x2++];
-		} 
-	}
-}
-
+public class MergeSortExample
+{  
+void merge(int arr[], int beg, int mid, int end)  
+{  
+  
+int l = mid - beg + 1;  
+int r = end - mid;  
+  
+intLeftArray[] = new int [l];  
+intRightArray[] = new int [r];  
+  
+for (int i=0; i<l; ++i)  
+LeftArray[i] = arr[beg + i];  
+  
+for (int j=0; j<r; ++j)  
+RightArray[j] = arr[mid + 1+ j];  
+  
+  
+int i = 0, j = 0;  
+int k = beg;  
+while (i<l&&j<r)  
+{  
+if (LeftArray[i] <= RightArray[j])  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+}  
+else  
+{  
+arr[k] = RightArray[j];  
+j++;  
+}  
+k++;  
+}  
+while (i<l)  
+{  
+arr[k] = LeftArray[i];  
+i++;  
+k++;  
+}  
+  
+while (j<r)  
+{  
+arr[k] = RightArray[j];  
+j++;  
+k++;  
+}  
+}  
+  
+void sort(int arr[], int beg, int end)  
+{  
+if (beg<end)  
+{  
+int mid = (beg+end)/2;  
+sort(arr, beg, mid);  
+sort(arr , mid+1, end);  
+merge(arr, beg, mid, end);  
+}  
+}  
+public static void main(String args[])  
+{  
+intarr[] = {90,23,101,45,65,23,67,89,34,23};  
+MyMergeSort ob = new MyMergeSort();  
+ob.sort(arr, 0, arr.length-1);  
+  
+System.out.println("\nSorted array");  
+for(int i =0; i<arr.length;i++)  
+{  
+    System.out.println(arr[i]+"");  
+}  
+}  
+}  
